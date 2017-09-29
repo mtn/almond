55e37f0ff99253d9426874a9899dfcee26ac34f8
Michael Noronha <michaeltnoronha@gmail.com>
2017-09-28 22:00:37 -0500

    Got lexer working with really stupid hack

--- lexer.rb
+++ lexer.rb
@@ -1,12 +1,44 @@
 
+def isspace(c)
+    c.match(/^\s*$/)
+end
+
+def isalpha(c)
+    c.match(/^[[:alpha:]]$/)
+end
+
+def isalphanum(c)
+    c.match(/\A[[:alnum:]]+\z/)
+end
+
 def gettok
     lastChar = ' '
-    while lastChar =~ /^\s*$/
+    while isspace(lastChar)
         lastChar = STDIN.getc
+        STDIN.getc
     end
 
-    if lastChar =~ /^[[:alpha:]]$/
-        puts 'it was in the alphabet'
+    if isalpha(lastChar)
+        identifierStr = lastChar
+
+        lastChar = STDIN.getc
+        STDIN.getc
+        while isalphanum(lastChar)
+            identifierStr += lastChar
+            lastChar = STDIN.getc
+            STDIN.getc
+        end
+
+        if identifierStr == 'def'
+            puts 'got DEF'
+            return :tok_def
+        elsif identifierStr == 'extern'
+            puts 'got EXTERN'
+            return :tok_extern
+        else
+            puts 'got IDENT'
+            return :tok_identifier
+        end
     end
 end
 
