00710a65c29a7bd51d70c9ef61e2ff7c8bd67672
Michael Noronha <michaeltnoronha@gmail.com>
2017-09-30 11:52:11 -0500

    Rewrite expression types and add grammar

--- src/expr.rb
+++ src/expr.rb
@@ -1,12 +1,73 @@
 
-class ExprAST
-end
-
-class Prototype < ExprAST
+class Prototype
     def initialize(name,params)
         @name = name #str
         @params = params #[str]
     end
 end
 
+class Definition
+    def initialize(prototype,expr)
+        @proto = prototype #prototype
+        @expr = expr #expr
+    end
+end
+
+class Expr
+end
+
+class Number < Expr
+    def initialize(val)
+        @val = val
+    end
+
+    def inspect
+        "expr.num(#{@val})"
+    end
+end
+
+class Variable < Expr
+    def initialize(name)
+        @name = name
+    end
+
+    def inspect
+        "expr.var(#{@name})"
+    end
+end
+
+class Binary < Expr
+    def initialize(lexpr,op,rexpr)
+        @op = op
+        @lexpr = lexpr
+        @rexpr = rexpr
+    end
+
+    def inspect
+        "expr.binary(#{lexpr},#{@op},#{@rexpr})"
+    end
+end
+
+class Call < Expr
+    def initialize(name,exprs)
+        @name = name
+        @exprs = exprs
+    end
+
+    def inspect
+        "expr.call(#{@name},#{@exprs})"
+    end
+end
+
+class IfElse < Expr
+    def initialize(cond,texpr,fexpr)
+        @cond = cond
+        @texpr = texpr
+        @fexpr = fexpr
+    end
+
+    def inspect
+        "expr.ifelse(#{@cond},#{@texpr},#{@fexpr})"
+    end
+end
 
